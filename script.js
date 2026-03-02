87fa4dbe538e12c83b40015845a79cd27b2778be
genericmaster <rakhivhanithendo@gmail.com>
2026-03-02 15:37:57 +0200

    again

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ button.addEventListener("click", async () => {
   }
   const data = await response.json()
   
-  data.forEach( user=> {
-    
+function displaycountry(country){
+    document.getElementById('country-info').innerHTML = `
+    <h2>${country.name.common}</h2>
+    <p><strong>Capital:</strong> ${country.capital[0]}</p>
+    <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+    <p><strong>Region:</strong> ${country.region}</p>
+    <img src="${country.flags.svg}" alt="${country.name.common} flag">`
 
-    
+}
+displaycountry(data) ;    
+    }
+
+catch (error) {
+  console.error('no')
+}
+            
   });
 
 
-  }   
-});
+   
